1d54e4816a21956e6b68dca3b437030964b0e911
Justus Evenschor <j.evenschor@gmail.com>
2022-08-30 10:21:28 +0200

    not much of value. switching back to master

--- lewagon_x_femtasy/data/read.py
+++ lewagon_x_femtasy/data/read.py
@@ -5,8 +5,12 @@ def read_data():
     '''path = os.path.join(
         os.environ.get('LOCAL_DATA_PATH'), 'raw_data.xlsx'
     )'''
-    path = os.path.join('..', 'raw_data', 'raw_data.xlsx')
+    path = os.path.join(os.path.dirname(__file__)[:-4], 'raw_data', 'raw_data.xlsx')
     df = pd.read_excel(path)
     return df
+
+#print(os.path.getcwd(__file__))
+#print(os.path.dirname(__file__))
+#pwd or cwd
 
 print(read_data())
