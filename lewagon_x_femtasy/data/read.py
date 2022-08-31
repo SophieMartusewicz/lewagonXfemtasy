22ea8174abae31dd5cebd14fc2e2058af68a610f
Patrick Dreier <patrick3dreier@googlemail.com>
2022-08-31 12:20:20 +0200

    fix universal os.path

--- lewagon_x_femtasy/data/read.py
+++ lewagon_x_femtasy/data/read.py
@@ -5,18 +5,14 @@ def read_data():
     '''path = os.path.join(
         os.environ.get('LOCAL_DATA_PATH'), 'raw_data.xlsx'
     )'''
-    #test = os.path.dirname()
-    print(test)
-    path = os.path.join(os.path.dirname(__file__)[:-4], 'raw_data', 'raw_data.xlsx')
+    path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), 'raw_data', 'raw_data.xlsx')
+    #print(os.__file__)
     print(path)
-    path = '/Users/pat3/code/SophieMartusewicz/lewagonXfemtasy/raw_data/raw_data.xlsx'
+
+    #path = '/Users/pat3/code/SophieMartusewicz/lewagonXfemtasy/raw_data/raw_data.xlsx'
     #path = '../../raw_data/raw_data.xlsx'
-    print(path)
+    #print(path)
     df = pd.read_excel(path)
     return df
-
-#print(os.path.getcwd(__file__))
-#print(os.path.dirname(__file__))
-#pwd or cwd
 
 print(read_data())
